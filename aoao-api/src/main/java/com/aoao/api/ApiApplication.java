d4db8ac61461f968d269e6bc3e3bb1f52c83844c
zhuruisong <zhuruisong@daishumovie.com>
2017-09-05 10:53:13 +0800

    init

---
+++ aoao-api/src/main/java/com/aoao/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.aoao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuruisong on 2017/9/5
+ * @since 1.0
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
